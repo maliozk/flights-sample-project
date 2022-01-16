b336ccae553e7b80d9087cff1e5c429e37273ce7
maliozk <maliozkeskin@gmail.com>
2022-01-17 00:01:09 +0300

    webflux h2

--- backend/src/main/java/com/flight/sample/flight/FlightApplication.java
+++ backend/src/main/java/com/flight/sample/flight/FlightApplication.java
@@ -2,7 +2,9 @@ package com.flight.sample.flight;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.context.annotation.Profile;
+import org.springframework.context.event.EventListener;
+import org.springframework.stereotype.Component;
 
 @SpringBootApplication
 //@SpringBootApplication(exclude = DataSourceAutoConfiguration.class)
@@ -10,6 +12,27 @@ public class FlightApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(FlightApplication.class, args);
+	}
+
+	@Component
+	public class H2 {
+
+		private org.h2.tools.Server webServer;
+
+		private org.h2.tools.Server tcpServer;
+
+		@EventListener(org.springframework.context.event.ContextRefreshedEvent.class)
+		public void start() throws java.sql.SQLException {
+			this.webServer = org.h2.tools.Server.createWebServer("-webPort", "8082", "-tcpAllowOthers").start();
+			this.tcpServer = org.h2.tools.Server.createTcpServer("-tcpPort", "9092", "-tcpAllowOthers").start();
+		}
+
+		@EventListener(org.springframework.context.event.ContextClosedEvent.class)
+		public void stop() {
+			this.tcpServer.stop();
+			this.webServer.stop();
+		}
+
 	}
 
 }
