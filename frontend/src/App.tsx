10344239674e47e2072904232f156241fce931d8
mozkeskin <mozkeskin@gmail.com>
2022-03-03 00:01:38 +0300

    map viewfix, records add

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,15 +1,39 @@
+import NeverTileDiscardPolicy from 'cesium/Source/Scene/NeverTileDiscardPolicy';
 import React from 'react'
 import { Header } from './header/header';
+import Records from './map/records';
 import MapView from './map/viewer';
-import { Menu } from './menu/menu';
+import Menu from './menu/menu';
+import "./App.css";
+import { MenuType } from './utils/Commons';
 
 function App() {
+
+  const [active, setActive] = React.useState(MenuType.MAP);
+
+  function change(menu: MenuType) {
+    setActive(menu);
+  }
+
+  function getPanel() {
+    switch (active) {
+      case MenuType.MAP:
+        return <MapView />;
+      case MenuType.RECORDS:
+        return <Records />;
+      default:
+        return null;
+    }
+  }
+
   return (
-    <>
-    <Header/>
-    <MapView/>
-    <Menu/>
-    </>
+    <div className='headerContainer'>
+      <Header />
+      <div className="bodyContainer">
+        <Menu callback={change} activeMenu={active} />
+        {getPanel()}
+      </div>
+    </div>
   )
 }
 
--- frontend/src/menu/menu.tsx
+++ frontend/src/menu/menu.tsx
@@ -1,13 +1,40 @@
 import * as React from "react";
 import "./menu.css"
 import imagg from "../resources/soccer.png"
+import { MenuType } from "../utils/Commons";
 
-export const Menu = () => (
+type MenuProps = {
+    callback: Function,
+    activeMenu: MenuType
+}
+
+
+function Menu(props: MenuProps) {
+
+  function menuUpdated(active:MenuType)
+  {
+    props.callback(active);
+  }
+
+  function isActive(selected:MenuType): string
+  {
+      if(selected === props.activeMenu)
+      {
+        return "item active";
+      }
+      return "item";
+
+  }
+
+  return (
   <>
   <div className="menu">
-    <li> <a className="item active" href="/">Map</a> </li>
-    <li> <a className="item" href="/records">Records</a> </li>
+    <li> <a className={props.activeMenu == MenuType.MAP ? "item active" : "item"}  href="#" onClick={() => menuUpdated(MenuType.MAP)}>Map</a> </li>
+    <li> <a className={props.activeMenu == MenuType.RECORDS ? "item active" : "item"}  href="#" onClick={() => menuUpdated(MenuType.RECORDS)}>Records</a> </li>
   </div>
-  <button className="menuButton"><img src={imagg}/></button>
- </> 
-); 
+  {/* <button className="menuButton"><img src={imagg}/></button> */}
+ </> )
+
+  }
+
+  export default Menu
